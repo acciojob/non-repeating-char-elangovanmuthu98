daf43098493e4f67a79eb5fb626e8c963a33d994
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2025-10-26 12:37:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+	for (let index = 0; index < str.length; index++) {
+		let char=str.charAt(index);
+		index++;
+		if(char==str.charAt(index) && index<str.length)
+		{
+		while(char==str.charAt(index) && index<str.length){
+			index++;
+		}
+			index--;
+			continue;
+		}
+		return char;
+
+		
+	}
+	return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
